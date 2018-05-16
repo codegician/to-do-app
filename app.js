b3cdf3c543ed9289e61a74085a19c7e63a5fce68
sharonchoi62 <sharonchoi62@gmail.com>
2018-05-15 21:35:42 -0700

    assignment-to-do-app-part1: completed

--- app.js
+++ app.js
@@ -13,12 +13,25 @@ function onReady () {
     let newLi = document.createElement('li');
     let checkbox = document.createElement('input');
 
+    let deleteBtn = document.createElement('button');
+    deleteBtn.textContent = "Delete";
+
+    deleteBtn.addEventListener("click", function(event){TODO_LIST.removeChild(this.parentElement)
+    });
+
     checkbox.type = "checkbox";
 
     newLi.textContent = title;
     newLi.appendChild(checkbox);
+    newLi.appendChild(deleteBtn);
 
     TODO_LIST.appendChild(newLi);
+
+    //Styling
+    newLi.className = "mdl-list__item";
+    checkbox.className = "mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect";
+    deleteBtn.className = "mdl-button mdl-js-button";
+    deleteBtn.style.padding = '0';
 
     NEW_TODO_TEXT .value = "";
   });
