8f81e751bb3b9829192fe5aae60c5778ac10bedc
sharonchoi62 <sharonchoi62@gmail.com>
2018-05-16 15:36:10 -0700

    checkpoint-to-do-app-part2: completed

--- app.js
+++ app.js
@@ -1,25 +1,52 @@
-window.onload = function () {
-  alert("The window has loaded!");
-  onReady();
-};
 
 function onReady () {
+
+  //Empty Array
+  let toDos = [];
+
+  //State
+  function createNewToDo(){
+    const NEW_TODO_TEXT = document.getElementById("newToDoText");
+
+    if(!NEW_TODO_TEXT.value){return; }
+
+    toDos.push({
+      title: NEW_TODO_TEXT.value,
+      complete: false
+    });
+
+    NEW_TODO_TEXT.value = "";
+
+    renderTheUI();
+  }
+
+  //UI
+  function renderTheUI(){
+    const TODO_LIST = document.getElementById("toDoList");
+
+    TODO_LIST.textContent = "";
+
+    toDos.forEach(function(toDo){
+      const NEW_LI = document.createElement('li');
+      const CHECKBOX = document.createElement('input');
+      CHECKBOX.type = "checkbox";
+
+      NEW_LI.textContent = toDo.title;
+      TODO_LIST.appendChild(NEW_LI);
+      NEW_LI.appendChild(CHECKBOX);
+    });
+  }
+
+  //Event Listener
   const ADD_TODO_FORM = document.getElementById("addToDoForm");
-  const NEW_TODO_TEXT = document.getElementById("newToDoText");
-  const TODO_LIST = document.getElementById("toDoList");
+  ADD_TODO_FORM.addEventListener("submit", event =>{
+    event.preventDefault();
+    createNewToDo();
+  })
 
-  ADD_TODO_FORM.addEventListener("submit", event => {event.preventDefault();
-    let title = NEW_TODO_TEXT.value;
-    let newLi = document.createElement('li');
-    let checkbox = document.createElement('input');
-
-    checkbox.type = "checkbox";
-
-    newLi.textContent = title;
-    newLi.appendChild(checkbox);
-
-    TODO_LIST.appendChild(newLi);
-
-    NEW_TODO_TEXT .value = "";
-  });
+  renderTheUI();
 }
+
+window.onload = function () {
+  onReady();
+};
